2a8e06225d871e625207904ae7ff070d320c9119
Bobby Johnson <bobby.johnson@gmail.com>
2012-02-18 20:09:26 -0800

    implemented table view

--- src/Furnishly.UI/Screens/ProductsListScreen.cs
+++ src/Furnishly.UI/Screens/ProductsListScreen.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Drawing;
 using System.Collections.Generic;
 
@@ -32,10 +33,11 @@ namespace Furnishly.UI
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
-			var products = GetProducts();
-			foreach (var product in products) {
+			var products = GetProducts().ToList();
 					
-			}
+			this.productsList.DataSource = new ProductTableViewDataSource(products);
+			this.productsList.Delegate = new ProductTableViewDelegate(this, products);
+			
 		}
 		
 		public override void ViewDidUnload()
@@ -56,5 +58,57 @@ namespace Furnishly.UI
 			return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
 		}
 	}
+	
+	public class ProductTableViewDataSource : UITableViewDataSource
+    {
+		private string cellId = "cellid";
+		private IList<Product> products;
+
+		public ProductTableViewDataSource(IList<Product> products)
+        {
+			this.products = products;
+        }
+
+        public override int RowsInSection(UITableView tableview, int section)
+        {
+            return products.Count;
+        }
+
+        public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
+        {
+        	var cell = tableView.DequeueReusableCell (cellId);
+        	if (cell == null)
+            {
+        		cell = new UITableViewCell (UITableViewCellStyle.Subtitle, cellId);
+        	}
+        	//cell.TextLabel.Font = UIFont.FromName ("Helvetica", 14.0f);
+        	//cell.TextLabel.LineBreakMode = UILineBreakMode.TailTruncation;
+        	cell.TextLabel.Text = products[indexPath.Row].Title;
+        	//cell.DetailTextLabel.Font = UIFont.FromName ("Helvetica", 12.0f);
+        	cell.DetailTextLabel.Text = products[indexPath.Row].Price;
+			cell.Accessory = UITableViewCellAccessory.DisclosureIndicator;
+            return cell;
+        }
+    }		
+	
+    public class ProductTableViewDelegate : UITableViewDelegate
+    {
+		private ProductsListScreen controller;
+		private IList<Product> products;
+		
+        public ProductTableViewDelegate(ProductsListScreen controller, IList<Product> products)
+        {
+			this.controller = controller;
+			this.products = products;
+        }			
+		
+		public override void RowSelected (UITableView tableView, NSIndexPath indexPath)
+        {
+			var product = products[indexPath.Row];
+			
+			var productScreen = new ProductScreen { Product = product };
+			controller.NavigationController.PushViewController(productScreen, true);	
+		}
+    }
 }
 
