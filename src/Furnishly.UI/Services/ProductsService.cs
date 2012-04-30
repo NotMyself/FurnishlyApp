32976a65e67a4dc890f95d2ef65df700fccb20e1
Bobby Johnson <bobby.johnson@gmail.com>
2012-04-30 15:34:46 -0700

    added some simple caching to product service

--- src/Furnishly.UI/Services/ProductsService.cs
+++ src/Furnishly.UI/Services/ProductsService.cs
@@ -14,18 +14,35 @@ namespace Furnishly.UI
 	{
 		private const string CoordinatesUrl = "http://furnishly.com/product_api.php?lat={0}&lng={1}";
 		
+		IDictionary<CLLocationCoordinate2D, IEnumerable<Product>> productSearchResults;
+		
 		public ProductsService()
 		{
+			productSearchResults = new Dictionary<CLLocationCoordinate2D, IEnumerable<Product>>();
+		}
+		
+		public void Clear()
+		{
+			productSearchResults.Clear();
 		}
 		
 		public IEnumerable<Product> GetProductsNear(CLLocationCoordinate2D location)
 		{
 			var products = Enumerable.Empty<Product>();
-			Console.WriteLine("Fetching products near: {0} lat, {1} long", location.Latitude, location.Longitude);
-			var request = WebRequest.Create(CoordinatesUrl.FormatWith(location.Latitude, location.Longitude));
-			using (var response = request.GetResponse())
+			
+			if(productSearchResults.ContainsKey(location))
 			{
-				products = JsonSerializer.DeserializeFromStream<Product[]>(response.GetResponseStream());
+				return productSearchResults[location];
+			}
+			else 
+			{
+				Console.WriteLine("Fetching products near: {0} lat, {1} long", location.Latitude, location.Longitude);
+				var request = WebRequest.Create(CoordinatesUrl.FormatWith(location.Latitude, location.Longitude));
+				using (var response = request.GetResponse())
+				{
+					products = JsonSerializer.DeserializeFromStream<Product[]>(response.GetResponseStream());
+				}
+				productSearchResults.Add(location, products);
 			}
 			
 			return products;
